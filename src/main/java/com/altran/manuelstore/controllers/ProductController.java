80a120c371415bd8e0f55f19b28c4638159f1e55
Jorge Godinho <jorge.godinho@ua.pt>
2019-10-03 10:11:33 +0100

    Adicionado GetById

--- src/main/java/com/altran/manuelstore/controllers/ProductController.java
+++ src/main/java/com/altran/manuelstore/controllers/ProductController.java
@@ -4,12 +4,10 @@ import com.altran.manuelstore.models.Produto;
 import com.altran.manuelstore.repository.ProductRepository;
 import org.apache.catalina.mapper.MapperListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,10 +17,27 @@ public class ProductController {
     @Autowired
     public ProductRepository productRepository;
 
+    // POST
     @PostMapping (consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity create(@RequestBody Produto produto){
         Produto p = productRepository.save(produto);
         return ResponseEntity.ok(p);
     }
+
+    // GET ALL
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getProducts() {
+        return ResponseEntity.ok(productRepository.findAll());
+    }
+
+    // GET BY ID
+    @GetMapping(path = "/{id}")
+    public ResponseEntity getById(@PathVariable Integer id){
+        return ResponseEntity.ok(productRepository.findById(id));
+    }
+
+    // PUT (atualizar)
+
+    // DELETE
 
 }
